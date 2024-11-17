0989722151724e3ce5b4f4e82b6aed4f3142791f
LanaSteen <l4n.steen@gmail.com>
2024-11-17 21:49:02 +0400

    FileStrem added

--- Lesson 18/Program.cs
+++ Lesson 18/Program.cs
@@ -1,10 +1,105 @@
-﻿namespace Lesson_18
+﻿using MiniBank.Models;
+using MiniBank.Repository;
+
+namespace Lesson_18
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //string filePath = "../../../test.txt";
+
+            string filePath = @"C:\\Users\\l4nst\\source\\repos\\Lesson 18\\MiniBank.Repository\\Data\\Customers.csv";
+
+            var repo = new CustomerFileStreamRepository(filePath);
+            var customer1 = new Customer
+            {
+                Name = "Someone Someone",
+                IdentityNumber = "123456789",
+                PhoneNumber = "599010101",
+                Email = "some@gmail.com",
+                Type = AccountType.Phyisical
+            };
+            repo.Create(customer1);
+
+            
+            var customers = repo.GetCustomers();
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"Id: {customer.Id}, Name: {customer.Name}, Email: {customer.Email}, Type: {customer.Type}");
+            }
+
+
+            var customerToUpdate = customers[10];
+            customerToUpdate.Name = "SOMEONE UPDATED";
+            repo.Update(customerToUpdate);
+
+           
+            Console.WriteLine("Updated customer:");
+            var updatedCustomer = repo.GetCustomer(customerToUpdate.Id);
+            Console.WriteLine($"Id: {updatedCustomer.Id}, Name: {updatedCustomer.Name}, Email: {updatedCustomer.Email}, Type: {updatedCustomer.Type}");
+
+         
+            var customerToDelete = customers[1];
+            repo.Delete(customerToDelete.Id);
+
+          
+            Console.WriteLine("Customers after update and delete:");
+            var restData = repo.GetCustomers();
+            foreach (var customer in restData)
+            {
+                Console.WriteLine($"Id: {customer.Id}, Name: {customer.Name}, Email: {customer.Email}, Type: {customer.Type}");
+            }
+
+            //StreamWriter ფაილში ჩაწერა
+            //using (StreamWriter sw = new StreamWriter(filePath, append: true))
+            //{
+            //    sw.WriteLine("Hello Lana");
+            //}
+
+
+            ////StreamReader ფაიილს წაკითხვა
+            //using (StreamReader sr = new(filePath))
+            //{
+            //    var content = sr.ReadToEnd();
+            //}
+
+
+            //FileStream ფაილში ჩაწერა
+            //try
+            //{
+            //    using (FileStream fileStreamWriter = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+            //    {
+            //        byte[] data = Encoding.UTF8.GetBytes("Hello World");
+            //        fileStreamWriter.Write(data, 0, data.Length);
+
+            //        Console.WriteLine("Data written in file.");
+
+            //    }// თავისით გამოიძახოს Dispose
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+
+            //FileStream ფაილის წაკითხვა
+            //try
+            //{
+            //    using (FileStream fileStreamReader = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            //    {
+            //        byte[] buffer = new byte[fileStreamReader.Length];
+            //        fileStreamReader.Read(buffer, 0, buffer.Length);
+            //        string realData = Encoding.UTF8.GetString(buffer);
+
+            //        Console.WriteLine(realData);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
         }
     }
 }
---
+++ MiniBank.Repository/CustomerFileStreamRepository.cs
@@ -0,0 +1,107 @@
+﻿using MiniBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBank.Repository
+{
+    public class CustomerFileStreamRepository
+    {
+        private readonly string _filePath;
+        private List<Customer> _customers;
+
+        public CustomerFileStreamRepository(string filePath)
+        {
+            _filePath = filePath;
+            _customers = LoadData();
+        }
+
+        public List<Customer> GetCustomers() => _customers;
+
+        public Customer GetCustomer(int id) => _customers.FirstOrDefault(person => person.Id == id);
+
+        public void Create(Customer customer)
+        {
+            customer.Id = _customers.Any() ? _customers.Max(c => c.Id) + 1 : 1;
+            _customers.Add(customer);
+            SaveData();
+        }
+
+        public void Update(Customer customer)
+        {
+            var index = _customers.FindIndex(c => c.Id == customer.Id);
+            if (index >= 0)
+            {
+                _customers[index] = customer;
+                SaveData();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var customer = _customers.FirstOrDefault(person => person.Id == id);
+            if (customer != null)
+            {
+                _customers.Remove(customer);
+                SaveData();
+            }
+        }
+
+        private void SaveData()
+        {
+        
+            using (var fileStream = new FileStream(_filePath, FileMode.Create, FileAccess.Write))
+            using (var writer = new StreamWriter(fileStream, Encoding.UTF8))
+            {
+             
+                writer.WriteLine("Id,Name,IdentityNumber,PhoneNumber,Email,Type");
+
+          
+                foreach (var customer in _customers)
+                {
+                    writer.WriteLine($"{customer.Id},{customer.Name},{customer.IdentityNumber},{customer.PhoneNumber},{customer.Email},{customer.Type}");
+                }
+            }
+        }
+
+        private List<Customer> LoadData()
+        {
+           
+            if (!File.Exists(_filePath))
+                return new List<Customer>();
+
+            var customers = new List<Customer>();
+
+    
+            using (var fileStream = new FileStream(_filePath, FileMode.Open, FileAccess.Read))
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+           
+                reader.ReadLine();
+
+           
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(',');
+                    var customer = new Customer
+                    {
+                        Id = int.Parse(parts[0]),
+                        Name = parts[1],
+                        IdentityNumber = parts[2],
+                        PhoneNumber = parts[3],
+                        Email = parts[4],
+                        Type = Enum.Parse<AccountType>(parts[5])
+                    };
+                    customers.Add(customer);
+                }
+            }
+
+            return customers;
+        }
+
+    }
+
+}
